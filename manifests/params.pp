a5261c876ca82d8702837f018102876731d78637
Garhan Attebury <garhan.attebury@unl.edu>
2017-05-09 14:40:12 -0500

    added default template location to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,17 +2,19 @@
 
 class lcmaps::params {
 
-    $conf_file               = '/etc/lcmaps.db'
-    $enable_posix_enf        = true
-    $gsi_authz_conf_file     = '/etc/grid-security/gsi-authz.conf'
-    $gsi_authz_conf_manage   = true
-    $gumsclient_cert         = '/etc/grid-security/hostcert.pem'
-    $gumsclient_endpoint     = 'https://yourgums.yourdomain:8443/gums/services/GUMSXACMLAuthorizationServicePort'
-    $gumsclient_key          = '/etc/grid-security/hostkey.pem'
-    $gumsclient_resourcetype = 'ce'
-    $package_name            = 'lcmaps'
-    $package_ensure          = 'present'
-    $package_plugins         = undef
-    $package_plugins_ensure  = 'present'
+    $conf_file                    = '/etc/lcmaps.db'
+    $conf_file_template           = 'lcmaps/lcmaps.db.erb'
+    $enable_posix_enf             = true
+    $gsi_authz_conf_file          = '/etc/grid-security/gsi-authz.conf'
+    $gsi_authz_conf_file_template = 'lcmaps/gsi-authz.conf.erb'
+    $gsi_authz_conf_manage        = true
+    $gumsclient_cert              = '/etc/grid-security/hostcert.pem'
+    $gumsclient_endpoint          = 'https://yourgums.yourdomain:8443/gums/services/GUMSXACMLAuthorizationServicePort'
+    $gumsclient_key               = '/etc/grid-security/hostkey.pem'
+    $gumsclient_resourcetype      = 'ce'
+    $package_name                 = 'lcmaps'
+    $package_ensure               = 'present'
+    $package_plugins              = undef
+    $package_plugins_ensure       = 'present'
 
 }
