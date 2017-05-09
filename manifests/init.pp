97d007d5610f02f794efe9c728cc9b4824efa324
Garhan Attebury <garhan.attebury@unl.edu>
2017-05-09 14:35:33 -0500

    added knobs for lcmaps and gsi-authz config file templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,17 @@
 # [*conf_file*]
 #   Location of configuration file for lcmaps. Default: /etc/lcmaps.db
 #
+# [*conf_file_template*]
+#   Template used to generate conf_file
+#
 # [*enable_posix_enf*]
 #   Enables the posix_enf enforcement plugin. Default: true
 #
 # [*gsi_authz_conf_file*]
 #   Location of gsi-authz.conf. Default: /etc/grid-security/gsi-authz.conf
+#
+# [*gsi_authz_conf_file_template*]
+#   Template used to generate gsi_authz_conf_file
 #
 # [*gsi_authz_conf_manage*]
 #   Whether to manage gsi-authz.conf. Default: true
@@ -56,23 +62,27 @@
 # Sample Usage:
 #
 class lcmaps (
-    $conf_file               = $lcmaps::params::conf_file,
-    $enable_posix_enf        = $lcmaps::params::enable_posix_enf,
-    $gsi_authz_conf_file     = $lcmaps::params::gsi_authz_conf_file,
-    $gsi_authz_conf_manage   = $lcmaps::params::gsi_authz_conf_manage,
-    $gumsclient_cert         = $lcmaps::params::gumsclient_cert,
-    $gumsclient_endpoint     = $lcmaps::params::gumsclient_endpoint,
-    $gumsclient_key          = $lcmaps::params::gumsclient_key,
-    $gumsclient_resourcetype = $lcmaps::params::gumsclient_resourcetype,
-    $package_name            = $lcmaps::params::package_name,
-    $package_ensure          = $lcmaps::params::package_ensure,
-    $package_plugins         = $lcmaps::params::package_plugins,
-    $package_plugins_ensure  = $lcmaps::params::package_plugins_ensure,
+    $conf_file                    = $lcmaps::params::conf_file,
+    $conf_file_template           = $lcmaps::params::conf_file_template,
+    $enable_posix_enf             = $lcmaps::params::enable_posix_enf,
+    $gsi_authz_conf_file          = $lcmaps::params::gsi_authz_conf_file,
+    $gsi_authz_conf_file_template = $lcmaps::params::gsi_authz_conf_file_template,
+    $gsi_authz_conf_manage        = $lcmaps::params::gsi_authz_conf_manage,
+    $gumsclient_cert              = $lcmaps::params::gumsclient_cert,
+    $gumsclient_endpoint          = $lcmaps::params::gumsclient_endpoint,
+    $gumsclient_key               = $lcmaps::params::gumsclient_key,
+    $gumsclient_resourcetype      = $lcmaps::params::gumsclient_resourcetype,
+    $package_name                 = $lcmaps::params::package_name,
+    $package_ensure               = $lcmaps::params::package_ensure,
+    $package_plugins              = $lcmaps::params::package_plugins,
+    $package_plugins_ensure       = $lcmaps::params::package_plugins_ensure,
     ) inherits lcmaps::params {
 
     validate_absolute_path($conf_file)
+    validate_string($conf_file_template)
     validate_bool($enable_posix_enf)
     validate_absolute_path($gsi_authz_conf_file)
+    validate_string($gsi_authz_conf_file_template)
     validate_bool($gsi_authz_conf_manage)
     validate_string($gumsclient_cert)
     validate_string($gumsclient_endpoint)
@@ -98,7 +108,7 @@ class lcmaps (
         group   => 'root',
         mode    => '0644',
         require => Package[$package_name],
-        content => template('lcmaps/lcmaps.db.erb'),
+        content => template($conf_file_template),
     }
 
     if $gsi_authz_conf_manage {
@@ -107,7 +117,7 @@ class lcmaps (
             owner   => 'root',
             group   => 'root',
             mode    => '0644',
-            content => template('lcmaps/gsi-authz.conf.erb'),
+            content => template($gsi_authz_conf_file_template),
         }
     }
 
